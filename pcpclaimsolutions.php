ab3b4ac123a9b7e35b6e0c14edf1843825c399c7
webozza <webozza@gmail.com>
2024-09-21 14:25:25 +0600

    cache cleared - version update

--- pcpclaimsolutions.php
+++ pcpclaimsolutions.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PCPCLAIMSOLUTIONS_VERSION', '1.0.12' );
+define( 'PCPCLAIMSOLUTIONS_VERSION', '1.0.15' );
 
 /**
  * The code that runs during plugin activation.
